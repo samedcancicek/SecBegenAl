cfec3bacd5e7fa3314efa8a86a0e4897d3f1d55b
hamdiye <hamdyekaya@gmail.com>
2018-04-23 16:55:12 +0300

    Add files via upload

---
+++ SecBegenAl/SecBegenAl/Profil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SecBegenAl
+{
+    public partial class Profil : UserControl
+    {
+        public Profil()
+        {
+            InitializeComponent();
+        }
+
+        public void profilGoruntule() {
+
+            this.BringToFront();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Profil_Load(object sender, EventArgs e)
+        {
+        //    ComboboxDoldur()
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
